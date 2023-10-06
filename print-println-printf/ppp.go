e36370a0717e21270eafe329ba297d988b24bf78
malvinval <malvinval09@gmail.com>
2023-10-06 12:02:44 +0700

    add: print-println-printf

---
+++ print-println-printf/ppp.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare variable using shorthand
+	a := "Gophers"
+
+	// print
+	fmt.Print(a)
+
+	// println
+	fmt.Println(a)
+
+	// printf
+	fmt.Printf("Data: %v, Type: %T", a, a)
+}
